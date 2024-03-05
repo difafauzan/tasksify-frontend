68bea6e247cadfb0dd364b253a2bd10c0576f836
Dwi Bagia Santosa <dwibagiasentosa@gmail.com>
2024-03-05 11:09:59 +0700

    Adding JWT header

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import { response } from 'express';
@@ -12,7 +12,8 @@ export class ApiService {
     private http: HttpClient,
     private response: Response,
     private cookies: CookieService
-  ) {}
+  ) {
+  }
   // // api task
   // postTask(data: any) {
   //   return this.http.post<any>('http://192.168.1.19:3000/task', data).pipe(
@@ -56,16 +57,32 @@ export class ApiService {
   }
 
   getBoard(userId: number) {
+    // Set access token
+    let accessToken = this.cookies.get('access-token')
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '+accessToken
+    });
+    let options = {headers: headers};
+    
     let data = { userId: userId };
-    return this.http.post('http://192.168.1.19:3000/board', data).pipe(
+    return this.http.post('http://192.168.1.19:3000/board', data, options).pipe(
       map((response: any) => {
         console.log(response);
         return response;
       })
     );
   }
-  getBoardById(id: any) {
-    return this.http.get<any>('http://192.168.1.19:3000/board/' + id).pipe(
+
+  getBoardById(id: any) {    // Set access token
+    let accessToken = this.cookies.get('access-token')
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '+accessToken
+    });
+    let options = {headers: headers};
+
+    return this.http.get<any>('http://192.168.1.19:3000/board/' + id, options).pipe(
       map((response: any) => {
         return response;
       })
